c12a22a0d18d1690313fa31bc1ddd900195a47a1
Milan <takacs.milan.29@gmail.com>
2024-11-14 21:00:21 +0100

    Add base class for data creation

--- Forgalom_Szenzor/Program.cs
+++ Forgalom_Szenzor/Program.cs
@@ -2,11 +2,17 @@
 using System.IO;
 using System.Collections.Generic;
 
-namespace Forgalom_Szenzor
+namespace Adateloallitas
 {
     class Forgalom
     {
-
+        private int id {  get; set; }
+        private string address { get; set; }
+        private string day { get; set; }
+        private int novehicles { get; set; }
+        private double avgspeed { get; set; }
+        private double temperature { get; set; }
+        private bool roadblock { get; set; }
     }
 
     internal class Program
