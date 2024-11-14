035b4a612b8b0239bd3f8019984bba75e399f29d
Milan <Milan@DESKTOP-NSSC1P9>
2024-11-14 20:48:51 +0100

    Add project files.

---
+++ Forgalom_Szenzor/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Forgalom_Szenzor
+{
+    class Forgalom
+    {
+
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
