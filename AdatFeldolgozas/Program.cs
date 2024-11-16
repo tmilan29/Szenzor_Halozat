666a2845a89da8b4e11c3a045a56a07ce7bf9594
Milan <takacs.milan.29@gmail.com>
2024-11-16 17:28:27 +0100

    Eseménykezelés hozzáadása

--- AdatFeldolgozas/Program.cs
+++ AdatFeldolgozas/Program.cs
@@ -9,30 +9,46 @@ namespace AdatFeldolgozas
 {
     internal class Program
     {
+        static void Event(string text)
+        {
+            Console.WriteLine(text);
+        }
+
         static List<Traffic.Traffic> Import()
         {
-            StreamReader input = new StreamReader("traffic.txt");
-            StringBuilder rows = new StringBuilder();
-            
-            while (!input.EndOfStream)
-            {
-                rows.Append(input.ReadLine());
-            }
-            
-            input.Close();
-            List<Traffic.Traffic> traffics = JsonConvert.DeserializeObject<List<Traffic.Traffic>>(rows.ToString());
-            
+            StreamReader json = new StreamReader("traffic.json");
+            string string_json = json.ReadToEnd();
+            json.Close();
+
+            List<Traffic.Traffic> traffics = JsonConvert.DeserializeObject<List<Traffic.Traffic>>(string_json.ToString());
+
             return traffics;
         }
         static void Main(string[] args)
         {
             List<Traffic.Traffic> traffics = Import();
 
+
+            //Az 1. ID-jú szenzor esetén a cím rosszul lett rögzítve a beolvasás során. Ezt javítjuk, melyet eseménykezelés kisér.
+            foreach (var t in traffics)
+            {
+                if (t.id == 1)
+                {
+                    t.EventHandlerForChange += Event;
+                    t.Address = "Holland fasor 2, Auchan";
+
+                };
+            }
+
             Console.WriteLine("A beolvasott adatok:");
             foreach (var t in traffics)
             {
                 Console.WriteLine(t.ToString());
             }
+
+
+
+
         }
     }
 }
