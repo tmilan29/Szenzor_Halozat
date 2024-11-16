2a945858e88dcd6de1ec69a0ef0481fcb5faeaad
Milan <takacs.milan.29@gmail.com>
2024-11-16 18:02:15 +0100

    Felhasználóbarát update

--- AdatFeldolgozas/Program.cs
+++ AdatFeldolgozas/Program.cs
@@ -28,25 +28,36 @@ namespace AdatFeldolgozas
         {
             List<Traffic.Traffic> traffics = Import();
 
-
-            //Az 1. ID-jú szenzor esetén a cím rosszul lett rögzítve a beolvasás során. Ezt javítjuk, melyet eseménykezelés kisér.
-            foreach (var t in traffics)
+            try
             {
-                if (t.id == 1)
+                Console.WriteLine("Az adatok beolvasása elkezdődött!\n");
+                //Az 1. ID-jú szenzor esetén a cím rosszul lett rögzítve a beolvasás során. Ezt javítjuk, melyet eseménykezelés kisér.
+                foreach (var t in traffics)
                 {
-                    t.EventHandlerForChange += Event;
-                    t.Address = "Holland fasor 2, Auchan";
+                    Console.WriteLine(t.ToString());
+                    if (t.id == 1)
+                    {
+                        t.EventHandlerForChange += Event;
+                        t.Address = "Holland fasor 2, Auchan";
 
-                };
+                    };
+                }
             }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            Console.WriteLine("\nTovábbhaladáshoz nyomj le egy karaktert!");
+            Console.ReadKey();
+            Console.Clear();
+            
 
             Console.WriteLine("A beolvasott adatok:");
             foreach (var t in traffics)
             {
                 Console.WriteLine(t.ToString());
             }
-
-
 
 
         }
