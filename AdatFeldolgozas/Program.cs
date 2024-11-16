40e9bfb79871c761715535d7dad359fe64142724
milan <takacs.milan.29@gmail.com>
2024-11-16 16:23:01 +0100

    JSON beolvasása

--- AdatFeldolgozas/Program.cs
+++ AdatFeldolgozas/Program.cs
@@ -1,10 +1,38 @@
-﻿namespace AdatFeldolgozas
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Traffic;
+using Newtonsoft.Json;
+
+namespace AdatFeldolgozas
 {
     internal class Program
     {
+        static List<Traffic.Traffic> Import()
+        {
+            StreamReader input = new StreamReader("traffic.txt");
+            StringBuilder rows = new StringBuilder();
+            
+            while (!input.EndOfStream)
+            {
+                rows.Append(input.ReadLine());
+            }
+            
+            input.Close();
+            List<Traffic.Traffic> traffics = JsonConvert.DeserializeObject<List<Traffic.Traffic>>(rows.ToString());
+            
+            return traffics;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<Traffic.Traffic> traffics = Import();
+
+            Console.WriteLine("A beolvasott adatok:");
+            foreach (var t in traffics)
+            {
+                Console.WriteLine(t.ToString());
+            }
         }
     }
 }
