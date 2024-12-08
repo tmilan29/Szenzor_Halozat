8690e3d666950d3b38296927cad4bc8e6a402041
Milan <takacs.milan.29@gmail.com>
2024-12-08 14:49:15 +0100

    DB kezelés

--- AdatFeldolgozas/Program.cs
+++ AdatFeldolgozas/Program.cs
@@ -84,9 +84,62 @@ namespace AdatFeldolgozas
             //Gergő és Milán közös - SQLite DB
             try
             {
-                using var connection = new SqliteConnection("Data Source=traffic.db");
+                var connection = new SqliteConnection("Data Source=traffic.db");
+                connection.Open();
                 Console.WriteLine("Adatbázishoz sikeresen kapcsolódva.");
 
+                var dropTable = @"DROP TABLE traffic";
+                using var tableDrop = new SqliteCommand(dropTable, connection);
+                tableDrop.ExecuteNonQuery();
+
+                var createTable = @"CREATE TABLE IF NOT EXISTS traffic(
+                            id INTEGER NOT NULL,
+                            address TEXT NOT NULL,
+                            day TEXT NOT NULL,
+                            noveh INTEGER NOT NULL,
+                            avgspeed REAL NOT NULL,
+                            roadblock NUMERIC NOT NULL
+                        )";
+
+                using var tableCreation = new SqliteCommand(createTable, connection);
+                tableCreation.ExecuteNonQuery();
+                Console.WriteLine("A tábla sikeresen elkészült.");
+
+                var insertValues = "INSERT INTO traffic (id, address, day, noveh, avgspeed, roadblock) " +
+                          "VALUES (@id, @address, @day, @noveh, @avgspeed, @roadblock)";
+                foreach (var t in traffics)
+                {
+                    using var insert = new SqliteCommand(insertValues, connection);
+
+                    insert.Parameters.AddWithValue("@id", t.id);
+                    insert.Parameters.AddWithValue("@address", t.address);
+                    insert.Parameters.AddWithValue("@day", t.day);
+                    insert.Parameters.AddWithValue("@noveh", t.novehicles);
+                    insert.Parameters.AddWithValue("@avgspeed", t.avgspeed);
+                    insert.Parameters.AddWithValue("@roadblock", t.roadblock);
+                    var rowInserted = insert.ExecuteNonQuery();
+                }
+
+                var select = "SELECT * FROM traffic";
+                using var read = new SqliteCommand(select, connection);
+                using var reader = read.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    Console.WriteLine("ID\tCím\t\t\t\tNap\tAutók száma\tÁtlag seb.\tÚtzár");
+                    while (reader.Read())
+                    {
+                        var id = reader.GetInt32(0);
+                        var address = reader.GetString(1);
+                        var day = reader.GetString(2);
+                        var novehicles = reader.GetInt32(3);
+                        var avgspeed = reader.GetDouble(4);
+                        var roadblock = reader.GetBoolean(5);
+                        Console.WriteLine($"{id}\t{address}\t\t{day}\t{novehicles}\t{avgspeed}\t{roadblock}");
+                    }
+                }
+
+                connection.Close();
             }
             catch(SqliteException e)
             {
